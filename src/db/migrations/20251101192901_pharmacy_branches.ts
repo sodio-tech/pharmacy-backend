bf6f51bf98f88d19057c00d5923019563750d129
vladi <aizisunanda@gmail.com>
2025-11-02 17:36:48 +0530

    removing super_admin from pharmacy_branches

--- src/db/migrations/20251101192901_pharmacy_branches.ts
+++ src/db/migrations/20251101192901_pharmacy_branches.ts
@@ -5,16 +5,12 @@ export async function up(knex: Knex): Promise<void> {
     table.increments("id").primary();
     table.integer("pharmacy_id").notNullable();
     table.foreign("pharmacy_id").references("pharmacies.id");
-    table.integer("super_admin").notNullable().unique();
-    table.foreign("super_admin").references("users.id");
     table.string("branch_name");
     table.string("branch_location");
     table.string("drug_license_number");
     table.timestamps(true, true);
 
     table.index('pharmacy_id');
-    table.index('super_admin');
-    table.foreign(["pharmacy_id", "super_admin"]).references(["pharmacies.id", "pharmacies.super_admin"]);
   })
 }
 

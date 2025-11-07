d33c70e0383a0112cde4b69f142a4a41b02c0db2
vladi <aizisunanda@gmail.com>
2025-11-07 19:30:34 +0530

    cors modified

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,25 @@ dotenv.config();
 
 const app = express();
 app.use(helmet());
-app.use(cors());
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://pharmy.sodio.tech"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());

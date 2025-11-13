7b0ffc8ffc450a0c69ab67c70a36d890c4ec23f6
vladi <aizisunanda@gmail.com>
2025-11-13 11:33:31 +0530

    plugged the validator for customer details

--- src/routes/v1/customerRoutes.ts
+++ src/routes/v1/customerRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import {verifyAccessToken} from "../../middleware/verifyAccessToken.js";
-import { validator, newCustomerSchema } from "../../middleware/validatorMiddleware.js";
+import { validator, newCustomerSchema, updateCustomerSchema } from "../../middleware/validatorMiddleware.js";
 // import { verifyRoleAccess } from "../../middleware/verifyRoleAccess.js";
 // import { PermissionMap } from '../../config/constants.js';
 import * as customerController from "../../controllers/customerController.js";
@@ -17,6 +17,6 @@ router.get("/details/:customer_id", customerController.getCustomerDetails);
 
 router.get("/prescriptions", customerController.getPrescriptions);
 
-router.put("/update-customer/:customer_id", customerController.updateCustomer);
+router.put("/update-customer/:customer_id", validator(updateCustomerSchema), customerController.updateCustomer);
 
 export default router;

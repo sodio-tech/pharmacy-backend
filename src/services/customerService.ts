88e29ac0003bf988882d6c0d100d8e4621dd8fac
vladi <aizisunanda@gmail.com>
2025-11-14 18:57:38 +0530

    today bug fixed

--- src/services/customerService.ts
+++ src/services/customerService.ts
@@ -75,6 +75,8 @@ export const getCustomerDetailsService = async (customer_id: string, user) => {
 export const getPrescriptionsService = async (params, user) => {
   let {page, limit, search, start_date, end_date} = params;
   if (start_date == end_date) {
+    start_date = new Date(start_date);
+    end_date = new Date(end_date);
     start_date.setHours(0, 0, 0, 0);
     end_date.setHours(23, 59, 59, 999);
   }

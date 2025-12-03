e591e19e9f34df615b611fdb4bd9034dc18a407f
vladi <aizisunanda@gmail.com>
2025-12-03 14:40:50 +0530

    selling price with gst

--- src/services/inventoryService.ts
+++ src/services/inventoryService.ts
@@ -321,6 +321,8 @@ export const getBranchStockService = async (pharmacy_id: number, branch_id: numb
 
   for (const stock of branchStock) {
     stock.image = stock.image && getFileUrl(stock.image);
+    stock.unit_price = Number(stock.unit_price);
+    stock.unit_price = ( (stock.gst_rate/100) * stock.unit_price ) + stock.unit_price;
   }
 
   return { 
